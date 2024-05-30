985d280d5785fe3d5c2044e1a5f00c45fbe29b28
Aziza <aziza.elgoul1@gmail.com>
2024-05-30 12:21:40 +0200

    Initial Commit

---
+++ SideMenu/SideMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SideMenuApp.swift
+//  SideMenu
+//
+//  Created by KARMANI Aziza on 30/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct SideMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
